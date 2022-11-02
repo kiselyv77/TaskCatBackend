3c331a2a7a243cfe3f18107466903f7b3360b67a
Mi <vovo555vova444@gmail.com>
2022-11-02 21:49:16 +0300

    optimization

--- src/main/kotlin/com/taskapp/features/files/configureGetAvatar.kt
+++ src/main/kotlin/com/taskapp/features/files/configureGetAvatar.kt
@@ -8,13 +8,9 @@ import java.io.File
 
 fun Application.configureGetAvatar() {
     routing {
-        get("/getAvatar/{token}") {
-            val token = call.parameters["token"]
-            val tokens = TokensTable.getTokens()
-            val loginUser = tokens.last { it.token == token }.login // по токену определяем логин
-
-            val file = File("C:\\Users\\Mi\\Desktop\\serverFiles\\${loginUser}_avatar.jpg")
-
+        get("/getAvatar/{login}") {
+            val login = call.parameters["login"]
+            val file = File("C:\\Users\\Mi\\Desktop\\serverFiles\\${login}_avatar.jpg")
             call.respondFile(file)
         }
     }
